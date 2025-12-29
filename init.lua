529cd336441882c3b37c8381be15973861066415
Rafael Leal <rafael.leal@aalto.fi>
2025-12-29 21:48:01 +0200

    Update 20251229

--- init.lua
+++ init.lua
@@ -1,2 +1,5 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
+
+-- vim.g.loaded_python3_provider = "~/.opt/venvs/nvim-python/bin/python3.10"
+vim.g.python3_host_prog = "~/.opt/venvs/nvim-python/bin/python3.12"
---
+++ lua/plugins/noice.lua
@@ -0,0 +1,15 @@
+if true then
+  return {}
+end
+
+require("lualine").setup({
+  sections = {
+    lualine_x = {
+      {
+        require("noice").api.statusline.mode.get,
+        cond = require("noice").api.statusline.mode.has,
+        color = { fg = "#ff9e64" },
+      },
+    },
+  },
+})
